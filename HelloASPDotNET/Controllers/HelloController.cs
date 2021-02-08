5c3253cd0af2078a80a759667b0bdbae759698d0
Reddy Lewis <lewismd3283@gmail.com>
2021-02-07 23:14:36 -0600

    After first Views lesson

---
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloASPDotNET.Controllers
+{
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/hello")]
+        public IActionResult Welcome(string name = "World")
+        {
+            return Content($"<h1>Welcome to my app, {name}!</h1>", "text/html");
+        }
+    }
+}
